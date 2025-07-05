6d0ba1cdfbc0d28ffc3c238ca77b040fb19eaf9c
Si manglam <qwer09214@gmail.com>
2025-07-05 18:33:52 +0800

    Refactor BinaryOpNode code generation: enhance assignment handling and improve arithmetic operation naming

--- src/asts/BinaryOpNode.cpp
+++ src/asts/BinaryOpNode.cpp
@@ -1,5 +1,6 @@
 #include "Compiler.h"
 #include "asts/BinaryOpNode.h"
+#include "asts/VariableNode.h"
 
 BinaryOpNode::BinaryOpNode(BaseExpr* _lhs, char _op, BaseExpr* _rhs): lhs(_lhs), op(_op), rhs(_rhs) {}
 
@@ -9,19 +10,36 @@ BinaryOpNode::~BinaryOpNode() {
 
 
 Value* BinaryOpNode::codegen(Compiler& c) {
-    Value* lhsCode = lhs->codegen(c);
-    Value* rhsCode = rhs->codegen(c);
+
+    if (op == '=') {
+        if (typeid(*lhs) != typeid(VariableNode)) {
+            cerr << "Expect variable at lhs of assign operator" << endl;
+            return nullptr;
+        }
+        VariableNode* lid = (VariableNode*)lhs;
+        AllocaInst* var = c.localVariables[lid->getName()];
+        if (!var) {
+            cerr << "Unknow variable: " << lid->getName() << endl;
+            return nullptr;
+        }
+        c.Builder->CreateStore(rhs->codegen(c), var);
+        return var;
+    }
+    
+
+    llvm::Value* lhsCode = lhs->codegen(c);
+    llvm::Value* rhsCode = rhs->codegen(c);
     if (!lhsCode || !rhsCode)
         return nullptr;
     switch (op){
         case '+':
-            return c.Builder->CreateAdd(lhsCode, rhsCode);
+            return c.Builder->CreateAdd(lhsCode, rhsCode, "addtmp");
         case '-':
-            return c.Builder->CreateSub(lhsCode, rhsCode);
+            return c.Builder->CreateSub(lhsCode, rhsCode, "subtmp");
         case '*':
-            return c.Builder->CreateMul(lhsCode, rhsCode);
+            return c.Builder->CreateMul(lhsCode, rhsCode, "timetmp");
         case '/':
-            return c.Builder->CreateSDiv(lhsCode, rhsCode);
+            return c.Builder->CreateSDiv(lhsCode, rhsCode, "divtmp");
     }
     return nullptr;
     
