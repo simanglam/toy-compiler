da68e161dc7e32df0f4c3319778d6ae1816dac3f
Si manglam <qwer09214@gmail.com>
2025-07-21 01:19:35 +0800

    Add LLVM target initialization and output file generation in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 #include <string>
 #include <map>
 
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/TargetParser/Host.h"
+#include "llvm/IR/LegacyPassManager.h"
+
 #include "include/Scanner.h"
 #include "include/Token.h"
 #include "include/Parser.h"
@@ -15,12 +24,48 @@ int main(int argc, char ** argv){
     Scanner s(filename);
     Parser p(s);
     Compiler c(p);
-    Token t = {};
     BaseExpr* ast = nullptr;
+    auto TargetTriple = sys::getDefaultTargetTriple();
+    InitializeAllTargetInfos();
+    InitializeAllTargets();
+    InitializeAllTargetMCs();
+    InitializeAllAsmParsers();
+    InitializeAllAsmPrinters();
+    std::string Error;
+    auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+
+    if (!Target) {
+        errs() << Error;
+        return 1;
+    }
+    
+    auto CPU = "generic";
+    auto Features = "";
+
+    TargetOptions opt;
+    auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, Reloc::PIC_);
+    c.TheModule->setDataLayout(TargetMachine->createDataLayout());
+    c.TheModule->setTargetTriple(TargetTriple);
 
     while ((ast = p.parseLine()) != nullptr){
         ast->codegen(c);
         delete ast;
     }
     c.TheModule.get()->print(outs(), nullptr);
+    auto outputFilename = "output.o";
+    std::error_code EC;
+    raw_fd_ostream dest(outputFilename, EC, sys::fs::OF_None);
+
+    if (EC) {
+        errs() << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    legacy::PassManager pass;
+    auto FileType = CodeGenFileType::AssemblyFile;
+
+    if (TargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
+        errs() << "TargetMachine can't emit a file of this type";
+        return 1;
+    }
 }
