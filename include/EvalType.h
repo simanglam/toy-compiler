0392d7a7ffc9e9688a5ce01c2a54b991447b8c21
Si manglam <qwer09214@gmail.com>
2025-08-01 19:46:23 +0800

    Add EvalType enum definition and include in BaseExpr for type consistency

---
+++ include/EvalType.h
@@ -0,0 +1,8 @@
+#ifndef __EvalType_Header__
+#define __EvalType_Header__
+
+enum EVALTYPE {
+    UNDIFINED, INTEGER, FLOAT, BOOL
+};
+
+#endif
--- include/asts/BaseExpr.h
+++ include/asts/BaseExpr.h
@@ -2,15 +2,12 @@
 #define __BaseExpr_HEADER__
 #include <string>
 #include <llvm/IR/Value.h>
+#include "EvalType.h"
 using namespace llvm;
 using namespace std;
 
 class Compiler;
 class Analyser;
-
-enum EVALTYPE {
-    UNDIFINED, INTEGER, FLOAT, BOOL
-};
 
 class BaseExpr{
 public:
