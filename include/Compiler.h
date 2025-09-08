0c010f61c6181de37f523d125eca45a68973bb08
Si manglam <qwer09214@gmail.com>
2025-09-08 20:54:36 +0800

    Fix missing llvm namespace in Compiler.h for IRBuilder, Function, AllocaInst, and GlobalVariable types.

--- include/Compiler.h
+++ include/Compiler.h
@@ -17,15 +17,15 @@ class Compiler {
 public:
     llvm::LLVMContext* TheContext;
     llvm::Module* TheModule;
-    IRBuilder<>* Builder;
+    llvm::IRBuilder<>* Builder;
     std::map<std::string, llvm::AllocaInst*> localVariables;
     std::map<std::string, llvm::GlobalVariable*> globalVar;
-    Function* currentFunction;
+    llvm::Function* currentFunction;
     bool compile();
     bool writeToFile(outputType, string&);
 
-    AllocaInst* allocateVar(llvm::Type*, string&, Value* = nullptr);
-    GlobalVariable* allocateGlobalVar(llvm::Type*, string&);
+    llvm::AllocaInst* allocateVar(llvm::Type*, string&, llvm::Value* = nullptr);
+    llvm::GlobalVariable* allocateGlobalVar(llvm::Type*, string&);
     Compiler(CommandLineOptions&);
     ~Compiler();
 };
